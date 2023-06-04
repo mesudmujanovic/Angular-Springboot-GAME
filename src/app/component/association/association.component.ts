d3c5f638cff241f08e2cc91dde296a60e0fb9cac
Mesud Mujanovic <mesud9494@gmail.com>
2023-06-04 17:05:22 +0200

    logic

--- src/app/component/association/association.component.ts
+++ src/app/component/association/association.component.ts
@@ -11,32 +11,97 @@ import { AssociationService } from 'src/app/service/association.service';
 export class AssociationComponent {
 
   allAssoc$: Observable<Association[]>
-  div1: string;
-  col1: string[];
-  
-  constructor( private assocService: AssociationService ){
+  randIndexAssoc: any;
+  itemText: { [key: string]: string[] };
+  columnAInput;
+  columnBInput;
+  columnCInput;
+  columnDInput;
+  showAllColumnAItems: boolean = false;
+
+  constructor(private assocService: AssociationService) {
+    this.itemText = { A: ["a1", "a2", "a3", "a4"], B: ["b1", "b2", "b3", "b4"], C: ["c1", "c2", "c3", "c4"], D: ["d1", "d2", "d3", "d4"] };
+
   }
 
-  getAllAssoc(): Observable<Association[]>{
+  showText(item: string, column: string, index: number): void {
+    this.itemText[column][index] = item;
+  }
+
+  handleInputChangeB(): void {
+    const solution = this.getSolutionForColumn('columnB');
+    if (this.columnBInput === solution) {
+      this.itemText = { B: this.randIndexAssoc.columnB }
+      console.log("tacno");
+    } else {
+      console.log("netacno");
+    }
+  }
+  handleInputChangeC(): void {
+    const solution = this.getSolutionForColumn('columnC');
+    if (this.columnCInput === solution) {
+      this.itemText = { C: this.randIndexAssoc.columnC }
+      console.log("tacno");
+    } else {
+      console.log("netacno");
+    }
+  }
+  handleInputChangeD(): void {
+    const solution = this.getSolutionForColumn('columnD');
+    if (this.columnDInput === solution) {
+      this.itemText = { D: this.randIndexAssoc.columnD }
+      console.log("tacno");
+    } else {
+      console.log("netacno");
+    }
+  }
+
+  handleInputChangeA(): void {
+    const solution = this.getSolutionForColumn('columnA');
+    if (this.columnAInput === solution) {
+      this.itemText = { A: this.randIndexAssoc.columnA }
+      console.log('Tačno');
+    } else {
+      console.log('Netačno');
+    }
+  }
+
+  getAllAssoc(): Observable<Association[]> {
     return this.allAssoc$ = this.assocService.getAll().pipe(
-      catchError( (error) =>{
-        console.log("error",error);
+      catchError((error) => {
+        console.log("error", error);
         return of([]);
       })
     )
   }
 
-  
-ngOnInit(): void {
-  this.getAllAssoc().pipe(
-    map(resp => resp.find(item => item.id === 6))
-  ).subscribe(assoc => {
-    console.log("response", assoc);
-    if (assoc) {
-      this.div1 = JSON.stringify(assoc.columnA);
-      this.col1 = JSON.parse(this.div1);
-      console.log("col1", this.col1);
+
+  ngOnInit(): void {
+    this.getAllAssoc().pipe(
+      map(resp => {
+        console.log("resp", resp);
+        const allAs = resp;
+        const random = Math.floor(Math.random() * allAs.length)
+        this.randIndexAssoc = allAs[random]
+        this.randIndexAssoc.columnA_solution = this.getSolutionForColumn('columnA');
+        this.randIndexAssoc.columnB_solution = this.getSolutionForColumn('columnB');
+        this.randIndexAssoc.columnC_solution = this.getSolutionForColumn('columnC');
+        this.randIndexAssoc.columnD_solution = this.getSolutionForColumn('columnD');
+        console.log("rand", this.randIndexAssoc);
+        console.log('columnA:', this.randIndexAssoc.columnA_solution);
+
+      })
+    ).subscribe(assoc => {
+    });
+  }
+
+  //prolazimo kroz solution i vracamo samo ono sto pocinje sa column da bi mogli da dodelimo gore povise svakoj koloni konacan rezultat
+  getSolutionForColumn(column: string): string {
+    const solution = this.randIndexAssoc.solutions.find(s => s.startsWith(column));
+    if (solution) {
+      return solution.split('_')[1];
     }
-  });
-}
+    return '';
+  }
+
 }
